ea15587394ddd30c2ba041791c3547d8ee98ae00
Vivek Gupta <vivekgupta86988@gmail.com>
2023-03-01 20:31:06 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello Vivek
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+      <Routes>
+        <Header/>
+        {/* <Route path="/" element={ } />
+        <Route path="/" element={ } /> */}
+      </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
